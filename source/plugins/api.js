0da3c04ec9f7f577290bd9efb7b3d9273180ba4b
Alexander Usolcev <alexander.usulcev@gmail.com>
2016-01-06 11:50:20 +0100

    Remove test method;

--- source/plugins/api.js
+++ source/plugins/api.js
@@ -289,9 +289,5 @@ PREGGIEAPI.load('device', 'utils', 'random').module('api', function(api) {
     };
 
 
-    API.prototype.testData = function(){
-        alert(123);
-    };
-
 	return this.publicateAPI("API", new API());
 });
