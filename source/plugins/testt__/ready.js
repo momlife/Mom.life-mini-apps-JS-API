037de3089cad230b7393c194dcae12e5455e152a
Alexander Usolcev <alexander.usolcev@gmail.com>
2016-02-05 15:29:43 +0100

    Remove old files;

--- source/plugins/testt__/ready.js
+++
@@ -1,8 +0,0 @@
-BBAPI.module("ready", function(api) {
-
-    var READY = function(callback){
-        return $(document).ready(callback)[0];
-    };
-
-    return this.publicateAPI("ready", READY);
-});
